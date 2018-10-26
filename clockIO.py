f767d9c7217393db5e4422348426278faba3f684
Vincenzo Sammartano <v.sammartano@gmail.com>
2018-10-26 15:43:38 +0100

    step2

--- clockIO.py
+++ clockIO.py
@@ -12,7 +12,7 @@ import tkinter as tk
 
 #Tkinter window
 root = tk.Tk() #new window
-root.geometry("700x470+100+50")
+root.geometry("450x450+100+50")
 root.title("Clock In/Out")
 root.resizable(width=False, height=False)
 
@@ -32,24 +32,37 @@ f_BO10 = ("arial",10,"bold")
 f_BO12 = ("arial",12,"bold")
 
 ##columnconfig
-rc = 40
+rc = 30
 for i in range(10):
     root.rowconfigure(i, minsize=rc)
-    
+
+weekHours = 37.5
+l0 = tk.Label(root,text="Week hours", padx = 10,font=f_BO10)   
+l0.grid(row=0,column=0)
+l0_1 = tk.Label(root,text="{}".format(weekHours),padx = 10,font=f_BO10)
+l0_1.grid(row=0,column=1)
+
+dayHours = 7.5
+
+
+
 #input part
 #Clock in/out selection    
-l0 = tk.Label(root,text="Clock In ", padx = 10,font=f_BO10)
-l0.grid(row=0,column=0,sticky="w")
-l0_1 = tk.Label(root,text="[hh:mm]",padx = 10,font=f_BO10)
-l0_1.grid(row=0,column=2)
+l1 = tk.Label(root,text="Clock In ", padx = 10,font=f_BO10)
+l1.grid(row=1,column=0,sticky="w")
+l1_1 = tk.Label(root,text="[hh:mm]",padx = 10,font=f_BO10)
+l1_1.grid(row=1,column=2)
 T_ = tk.StringVar()
 t1 = tk.Entry(root,textvariable= T_ , width=6,justify="center",font=f_10)
-t1.grid(row=0,column=1)
-t1.insert("end", 20)  
+t1.grid(row=1,column=1)
+t1.insert("end", "00:00")  
+
     
     
-    
-    
+ln = tk.Button(root,text="Exit",command=root.destroy,font=f_BO9)
+ln.config( height = 1, width = 15)
+ln.grid(row=10,column=3)
+
     
     
     
